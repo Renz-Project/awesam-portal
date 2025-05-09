93703b18af5745b4809ea8594150c210c2d22c76
renzcabato1 <cabato.renz.renz@gmail.com>
2025-05-09 16:58:25 +0800

    Done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.header')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="main-content">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="page-content">
+        <div class="container-fluid">
+
+            <!-- start page title -->
+            {{-- <div class="row">
+                <div class="col-12">
+                    <div class="page-title-box d-sm-flex align-items-center justify-content-between bg-galaxy-transparent">
+                        <h4 class="mb-sm-0">Analytics</h4>
+
+                        <div class="page-title-right">
+                            <ol class="breadcrumb m-0">
+                                <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboards</a></li>
+                                <li class="breadcrumb-item active">Analytics</li>
+                            </ol>
                         </div>
-                    @endif
 
-                    You are logged in!
+                    </div>
+                </div>
+            </div> --}}
+            <!-- end page title -->
+
+           
+
+        </div>
+        <!-- container-fluid -->
+    </div>
+    <!-- End Page-content -->
+
+    <footer class="footer">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-6">
+                    {{date('Y')}} © Awesame
+                </div>
+                <div class="col-sm-6">
+                    <div class="text-sm-end d-none d-sm-block">
+                        Design & Develop by 
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </footer>
 </div>
 @endsection
