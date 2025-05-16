9f09278379ba86f36b725da836041646e9c40b44
renzcabato1 <cabato.renz.renz@gmail.com>
2025-05-16 10:38:28 +0800

    Done

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,65 +1,51 @@
 @extends('layouts.app')
+
 @section('content')
-<div class="content">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <img src="{{asset('login_css/images/present.png')}}" alt="Image" class="img-fluid">
-        </div>
-        <div class="col-md-6 ">
-          <div class="row justify-content-center">
-            <div class="col-md-8">
-              <div class="mb-4">
-              <h3>Reset Password </h3>
-              <p class="mb-4"><strong>{{ config('app.name', 'Laravel') }}</strong></p>
-            </div>
-            <form method="POST" action="{{ route('password.email') }}" onsubmit='show()'>
-                @csrf
-              <div class="form-group first">
-                <label for="email">Email</label>
-                <input  class="form-control" id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email Address" required autofocus>
-              
-              </div>
-              @if($errors->any())
-                    <div class="form-group alert alert-danger alert-dismissable">
-                        <button aria-hidden="true" data-dismiss="alert" class="close" type="button">×</button>
-                        <strong>{{$errors->first()}}</strong>
-                    </div>
-                @endif
-                @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
+
+<div class="row justify-content-center">
+    <div class="col-md-8 col-lg-6 col-xl-5">
+        <div class="card mt-4 card-bg-fill">
+
+            <div class="card-body p-4">
+                <div class="text-center mt-2">
+                    <h5 class="text-primary">Forgot Password?</h5>
+                    <p class="text-muted">Reset password with SALTiii</p>
+
+                    <lord-icon src="https://cdn.lordicon.com/rhvddzym.json" trigger="loop" colors="primary:#0ab39c" class="avatar-xl"></lord-icon>
+
                 </div>
-              @endif
-              <input type="submit" value="{{ __('Send Password Reset Link') }}" class="btn text-white btn-block btn-primary">
-              <br>
-              <div class="d-flex mb-5 align-items-center">
-                {{-- <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator"></div>
-                </label> --}}
-                <span class="ml-auto"><a  href="{{ url('/login') }}" onclick='show()' class="forgot-pass">Back to Log in</a></span> 
-              </div>
-              {{-- <span class="d-block text-left my-4 text-muted text-right"> or sign in with</span>
-              
-              <div class="social-login text-right">
-                <a href="#" class="facebook">
-                  <span class="icon-facebook mr-3"></span> 
-                </a>
-                <a href="#" class="twitter">
-                  <span class="icon-twitter mr-3"></span> 
-                </a>
-                <a href="#" class="google">
-                  <span class="icon-google mr-3"></span> 
-                </a>
-              </div> --}}
-            </form>
+
+                <div class="alert border-0 alert-warning text-center mb-2 mx-2" role="alert">
+                    Enter your email and instructions will be sent to you!
+                </div>
+                <div class="p-2">
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+                        <div class="mb-4">
+                            <label class="form-label">Email</label>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                        </div>
+
+                        <div class="text-center mt-4">
+                            <button class="btn btn-success w-100" type="submit">Send Reset Link</button>
+                        </div>
+                        
+                        @if ($errors->has('email'))
+                            <div class="mt-3 form-group alert alert-danger alert-dismissable">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            <div>
+                        @endif
+                    </form><!-- end form -->
+                </div>
             </div>
-          </div>
-          
+            <!-- end card body -->
         </div>
-        
-      </div>
+        <!-- end card -->
+
+        <div class="mt-4 text-center">
+            <p class="mb-0">Wait, I remember my password... <a href="{{url('/')}}" class="fw-semibold text-primary text-decoration-underline"> Click here </a> </p>
+        </div>
+
     </div>
-  </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable" data-theme="default" data-theme-colors="default">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -8,32 +9,23 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="shortcut icon" href="{{url('images/aaa.png')}}">
-    <link rel="icon" href="{{url('images/aaa.png')}}">
-    <!-- Scripts -->
-    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    {{-- <link rel="stylesheet" href="{{asset('login_css/fonts/icomoon/style.css')}}">
-    <link rel="stylesheet" href="{{asset('login_css/css/owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('login_css/css/bootstrap.min.css')}}"> --}}
-    <link rel="stylesheet" href="{{asset('login_css/css/style.css')}}">
+    <link rel="shortcut icon"  href="{{url('images/aaa.png')}}">
+    <link rel="icon"  href="{{url('images/aaa.png')}}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="stylesheet" href="{{ asset('/body_css/vendors/feather/feather.css') }}">
-	<link rel="stylesheet" href="{{ asset('/body_css/vendors/ti-icons/css/themify-icons.css') }}">
-	<link rel="stylesheet" href="{{ asset('/body_css/vendors/css/vendor.bundle.base.css') }}">
-
-    <link rel="stylesheet" href="{{ asset('/body_css/vendors/datatables.net-bs4/dataTables.bootstrap4.css') }}">
-	<link rel="stylesheet" href="{{ asset('/body_css/vendors/ti-icons/css/themify-icons.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('/body_css/js/select.dataTables.min.css') }}">
-	<!-- Plugin css for this page -->
-	<link rel="stylesheet" href="{{ asset('/body_css/vendors/select2/select2.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('/body_css/vendors/select2-bootstrap-theme/select2-bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ asset('/body_css/css/vertical-layout-light/style.css') }}">
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    {{-- <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css"> --}}
+    <!-- Layout config Js -->
+    <script src="{{asset('inside_css/assets/js/layout.js')}}"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{asset('inside_css/assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{asset('inside_css/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{asset('inside_css/assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{asset('inside_css/assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
+ 
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
     <style>
         .loader {
             position: fixed;
@@ -46,197 +38,10 @@
             opacity: .8;
             background-size:120px 120px;
         }
-        .orangebox {
-			background-color: lightgrey;
-			width: 15px;
-			height: 15px;
-			border: 10px solid orange;
-			float: right;
-		}
-
-		.orangebox1 {
-			background-color: lightgrey;
-			width: 15px;
-			height: 15px;
-			border: 10px solid orange;
-			display: inline-block;
-		}
-
-		.green {
-			background-color: lightgrey;
-			width: 15px;
-			height: 15px;
-			border: 10px solid green;
-			display: inline-block;
-		}
-        /*Hide all except first fieldset*/
-		#msform fieldset:not(:first-of-type) {
-			display: none;
-		}
-
-		#msform fieldset .form-card {
-			text-align: left;
-			color: #9E9E9E;
-		}
-
-
-
-		#msform .action-button:hover,
-		#msform .action-button:focus {
-			box-shadow: 0 0 0 2px white, 0 0 0 3px skyblue;
-		}
-
-
-		#msform .action-button-previous:hover,
-		#msform .action-button-previous:focus {
-			box-shadow: 0 0 0 2px white, 0 0 0 3px #616161;
-		}
-
-		/*Dropdown List Exp Date*/
-		select.list-dt {
-			border: none;
-			outline: 0;
-			border-bottom: 1px solid #ccc;
-			padding: 2px 5px 3px 5px;
-			margin: 2px;
-		}
-
-		select.list-dt:focus {
-			border-bottom: 2px solid skyblue;
-		}
-
-		/*The background card*/
-		.card {
-			z-index: 0;
-			border: none;
-			border-radius: 0.5rem;
-			position: relative;
-		}
-
-		/*FieldSet headings*/
-		.fs-title {
-			font-size: 25px;
-			color: #2C3E50;
-			margin-bottom: 10px;
-			font-weight: bold;
-			text-align: left;
-		}
-
-		/*progressbar*/
-		#progressbar {
-			margin-bottom: 30px;
-			overflow: hidden;
-			color: lightgrey;
-		}
-
-		#progressbar .active {
-			color: #000000;
-		}
-
-		#progressbar li {
-			list-style-type: none;
-			font-size: 12px;
-			width: 25%;
-			float: left;
-			position: relative;
-		}
-
-		/*Icons in the ProgressBar*/
-		#progressbar #account:before {
-			font-family: FontAwesome;
-			content: "\f007";
-		}
-
-		#progressbar #personal:before {
-			font-family: FontAwesome;
-			content: "\f007";
-		}
-
-		#progressbar #payment:before {
-			font-family: FontAwesome;
-			content: "\f09d";
-		}
-
-		#progressbar #confirm:before {
-			font-family: FontAwesome;
-			content: "\f090";
-		}
-
-		.user:before {
-			font-family: FontAwesome;
-			content: "\f02d";
-		}
-
-		.employment:before {
-			font-family: FontAwesome;
-			content: "\f0f0";
-		}
-
-		/*ProgressBar before any progress*/
-		#progressbar li:before {
-			width: 50px;
-			height: 50px;
-			line-height: 45px;
-			display: block;
-			font-size: 18px;
-			color: #ffffff;
-			background: lightgray;
-			border-radius: 50%;
-			margin: 0 auto 10px auto;
-			padding: 2px;
-		}
-
-		/*ProgressBar connectors*/
-		#progressbar li:after {
-			content: '';
-			width: 100%;
-			height: 2px;
-			background: lightgray;
-			position: absolute;
-			left: 0;
-			top: 25px;
-			z-index: -1;
-		}
-
-		/*Color number of the step and the connector before it*/
-		#progressbar li.active:before,
-		#progressbar li.active:after {
-			background: skyblue;
-		}
-
-		/* width */
-		::-webkit-scrollbar {
-			width: 5px;
-		}
-
-		/* Track */
-		::-webkit-scrollbar-track {
-			background: #f1f1f1;
-		}
-
-		/* Handle */
-		::-webkit-scrollbar-thumb {
-			background: #888;
-		}
-
-		/* Handle on hover */
-		::-webkit-scrollbar-thumb:hover {
-			background: #555;
-		}
-
-		.tab-content {
-			padding: 20px;
-		}
-
-		input::-webkit-outer-spin-button,
-		input::-webkit-inner-spin-button {
-			-webkit-appearance: none;
-			margin: 0;
-		}
-
-		/* Firefox */
-		input[type=number] {
-			-moz-appearance: textfield;
+      .bg-overlay {
+		    background: linear-gradient(to right, #c3c3c3, #c3c3c3) !important;
+    		/* opacity: .9; */
+   			/* Change 5px to your desired thickness */
 		}
     </style>
     <!-- LogIN CSS -->
@@ -248,160 +53,56 @@
 <body>
     <div id = "loader" style="display:none;" class="loader">
     </div>
-    <div id="app">
-        <main class="py-4">
-            @yield('content')
-        </main>
+    
+    <div class="auth-page-wrapper pt-5">
+        <!-- auth page bg -->
+        <div class="auth-one-bg-position auth-one-bg" id="auth-particles">
+            <div class="bg-overlay"></div>
+
+            <div class="shape">
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1440 120">
+                    <path d="M 0,36 C 144,53.6 432,123.2 720,124 C 1008,124.8 1296,56.8 1440,40L1440 140L0 140z"></path>
+                </svg>
+            </div>
+        </div>
+
+        <!-- auth page content -->
+        <div class="auth-page-content">
+             <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="text-center mt-sm-5 mb-4 text-white-50">
+                            <div>
+                                <a href="{{url('/')}}" class="d-inline-block auth-logo">
+                                    <img src="{{asset('images/logo_mo.png')}}" class="white-border" alt="" height="50">
+                                </a>
+                            </div>
+                            {{-- <p class="mt-3 fs-15 fw-medium">3 in 1 solution</p> --}}
+                        </div>
+                    </div>
+                </div>
+                @yield('content')
+             </div>
+        </div>
     </div>
 
     {{-- <script src="{{asset('login_css/js/jquery-3.3.1.min.js')}}"></script>
     <script src="{{asset('login_css/js/popper.min.jss')}}"></script>
     <script src="{{asset('login_css/js/bootstrap.min.js')}}"></script>
     <script src="{{asset('login_css/js/main.js')}}"></script> --}}
+    <script src="{{asset('inside_css/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('inside_css/assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{asset('inside_css/assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src="{{asset('inside_css/assets/libs/feather-icons/feather.min.js')}}"></script>
+    <script src="{{asset('inside_css/assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
+    <script src="{{asset('inside_css/assets/js/plugins.js')}}"></script>
 
-    <script src="{{ asset('/body_css/vendors/js/vendor.bundle.base.js') }}"></script>
-	<!-- endinject -->
-	<!-- Plugin js for this page -->
-	<script src="{{ asset('/body_css/vendors/chart.js/Chart.min.js') }}"></script>
-
-	<script src="{{ asset('/body_css/vendors/select2/select2.min.js') }}"></script>
-
-    <script src="{{ asset('/body_css/js/dashboard.js') }}"></script>
-	<script src="{{ asset('/body_css/js/select2.js') }}"></script>
-
-
-	<script src="{{ asset('/body_css/vendors/datatables.net/jquery.dataTables.js') }}"></script>
-	<script src="{{ asset('/body_css/vendors/datatables.net-bs4/dataTables.bootstrap4.js') }}"></script>
-	<script src="{{ asset('/body_css/vendors/jquery.repeater/jquery.repeater.min.js') }}"></script>
-
-	<script src="{{ asset('/body_css/js/dataTables.select.min.js') }}"></script>
-
-	<script src="{{ asset('/body_css/js/off-canvas.js') }}"></script>
-	<script src="{{ asset('/body_css/js/hoverable-collapse.js') }}"></script>
-	<script src="{{ asset('/body_css/js/template.js') }}"></script>
-	<script src="{{ asset('/body_css/js/settings.js') }}"></script>
-	<script src="{{ asset('/body_css/js/todolist.js') }}"></script>
-
-	<script src="{{ asset('/body_css/js/tabs.js') }}"></script>
-	<script src="{{ asset('/body_css/js/form-repeater.js') }}"></script>
-	<script src="{{ asset('/body_css/vendors/sweetalert/sweetalert.min.js') }}"></script>
-    
-    <script src="{{ asset('/body_css/vendors/inputmask/jquery.inputmask.bundle.js') }}"></script>
-    <script src="{{ asset('/body_css/vendors/inputmask/jquery.inputmask.bundle.js') }}"></script>
-    <script src="{{ asset('/body_css/js/inputmask.js') }}"></script>
-    @include('sweetalert::alert')
-    <script type='text/javascript'>
-        function show() {
-            document.getElementById("loader").style.display="block";
-        }
-    </script>
-    <script>
-		$(document).ready(function() {
-
-			var current_fs, next_fs, previous_fs; //fieldsets
-			var opacity;
-
-			$(".next").click(function() {
-				current_fs = $(this).parent();
-				next_fs = $(this).parent().next();
-				var fld = $(this).closest("fieldset").attr('id');
-				// alert(fld);
-				var isValid = true;
-				var classname = 'required';
-				$('#' + fld + ' .' + classname + '').each(function(i, obj) {
-					if (obj.value == '') {
-						isValid = false;
-						return isValid;
-					}
-				});
-
-				if (!isValid) {
-					$('#' + fld + ' .' + classname + '').each(function(i, obj) {
-						if (obj.value == '') {
-
-							var d = (obj.className).includes("js-example-basic-single");
-							if (d == false) {
-								// return false;
-								obj.style.border = '1px solid red';
-							} else {
-
-								$("select[name='" + obj.getAttribute("name") + "']").next("span").css(
-									'border', '1px solid red');
-								console.log(obj.getAttribute("name"));
-							}
-						} else {
-							$("select[name='" + obj.getAttribute("name") + "']").next("span").css(
-								'border', '1px solid #CED4DA');
-							obj.style.border = '1px solid #CED4DA';
-						}
-					});
-				}
-				if (isValid) {
-
-
-					//Add Class Active
-					$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-					next_fs.show();
-					//hide the current fieldset with style
-					current_fs.animate({
-						opacity: 0
-					}, {
-						step: function(now) {
-							// for making fielset appear animation
-							opacity = 1 - now;
-
-							current_fs.css({
-								'display': 'none',
-								'position': 'relative'
-							});
-							next_fs.css({
-								'opacity': opacity
-							});
-						},
-						duration: 600
-					});
-				}
-				return isValid;
-
-			});
-
-			$(".previous").click(function() {
-
-				current_fs = $(this).parent();
-				previous_fs = $(this).parent().prev();
-
-				//Remove class active
-				$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-				//show the previous fieldset
-				previous_fs.show();
-
-				//hide the current fieldset with style
-				current_fs.animate({
-					opacity: 0
-				}, {
-					step: function(now) {
-						// for making fielset appear animation
-						opacity = 1 - now;
-
-						current_fs.css({
-							'display': 'none',
-							'position': 'relative'
-						});
-						previous_fs.css({
-							'opacity': opacity
-						});
-					},
-					duration: 600
-				});
-			});
-
-			$('.radio-group .radio').click(function() {
-				$(this).parent().find('.radio').removeClass('selected');
-				$(this).addClass('selected');
-			});
-
-		});
-	</script>
+    <!-- particles js -->
+    <script src="{{asset('inside_css/assets/libs/particles.js/particles.js')}}"></script>
+    <!-- particles app js -->
+    <script src="{{asset('inside_css/assets/js/pages/particles.app.js')}}"></script>
+    <!-- password-addon init -->
+    <script src="{{asset('inside_css/assets/js/pages/password-addon.init.js')}}"></script>
+  
 </body>
 </html>
