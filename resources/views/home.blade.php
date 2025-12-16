d86ad2670e1c1faa9da1a1d733683bc12100e6ed
renzcabato1 <cabato.renz.renz@gmail.com>
2025-12-16 14:09:11 +0800

    D

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -304,7 +304,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach($transactions->sortByDesc('id') as $transaction)
+                                @foreach($transactions->sortByDesc('id')->get('20') as $transaction)
                                 <tr>
                                     
                                     <td>#{{$transaction->id}}</td>
